349427776f343fa34c5203931d6e57fe2b465656
Andrii Oriekhov <andriyorehov@gmail.com>
2023-05-06 20:07:53 +0300

    support describe.only.each

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ const project = new Project({
 
 const jestGlobalApis = ["afterAll", "afterEach", "beforeAll", "beforeEach", "describe", "test", "it", "fit", "expect"];
 
+const testApiProps = ["concurrent", "each", "only", "skip", "todo", "failing"];
+const jestGlobalApiProps: Record<string, string[]> = {
+  describe: ["each", "only", "skip"],
+  fit: ["each", "failing"],
+  it: testApiProps,
+  test: testApiProps,
+};
+const jestGlobalApiPropsKeys = Object.keys(jestGlobalApiProps);
+
 const insertViteImport = (sourceFile: SourceFile) => {
   const callExpressions = sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression);
   const namedImport = [];
@@ -20,17 +29,54 @@ const insertViteImport = (sourceFile: SourceFile) => {
       const expressionText = expression.getText();
       namedImport.push(expressionText);
     }
+
     if (Node.isPropertyAccessExpression(expression)) {
       const propExpression = expression.getExpression();
-      if (propExpression.getText() === "jest") {
-        propExpression.replaceWithText("vi");
-        api.push("vi");
+
+      if (Node.isIdentifier(propExpression)) {
+        const propExpressionText = propExpression.getText();
+        const propExpressionName = expression.getName();
+
+        if (
+          jestGlobalApiPropsKeys.includes(propExpressionText) &&
+          jestGlobalApiProps[propExpressionText].includes(propExpressionName)
+        ) {
+          api.push(propExpressionText);
+        }
+
+        if (propExpressionText === "jest") {
+          propExpression.replaceWithText("vi");
+          api.push("vi");
+        }
+      }
+
+      // TODO: create functions
+      if (Node.isPropertyAccessExpression(propExpression)) {
+        const propExpressionNested = propExpression.getExpression();
+        if (Node.isIdentifier(propExpressionNested)) {
+          const propExpressionText = propExpressionNested.getText();
+          const propExpressionName = expression.getName();
+
+          if (
+            jestGlobalApiPropsKeys.includes(propExpressionText) &&
+            jestGlobalApiProps[propExpressionText].includes(propExpressionName)
+          ) {
+            api.push(propExpressionText);
+          }
+
+          if (propExpressionText === "jest") {
+            propExpressionNested.replaceWithText("vi");
+            api.push("vi");
+          }
+        }
       }
     }
   }
 
   const intersection = intersect(namedImport, jestGlobalApis);
-  const importDeclarationString = `import { ${[...intersection, ...api].sort().join(", ")} } from "vitest";`;
+  const importDeclarationString = `import { ${[...new Set([...intersection, ...api])]
+    .sort()
+    .join(", ")} } from "vitest";`;
 
   sourceFile.insertStatements(0, importDeclarationString);
 };
@@ -48,4 +94,4 @@ export const migrate = (path: string) => {
   return project.save();
 };
 
-// migrate('test/__fixtures__/**/*.input.{tsx,ts,js}');
+// migrate("test/__fixtures__/**/*.input.{tsx,ts,js}");
